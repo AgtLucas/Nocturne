d39a41caf3476a300123e73279f93bea17c66ad9
Lucas <lucas@agtlucas.com>
2015-03-18 10:28:18 -0300

    Adding TableViewCell object

---
+++ Nocturne/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Nocturne
+//
+//  Created by Lucas on 3/18/15.
+//  Copyright (c) 2015 AWLKA. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
