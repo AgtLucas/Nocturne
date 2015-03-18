6676471899a2835bf289c272a6b6751df4a2170c
Lucas <lucas@agtlucas.com>
2015-03-18 14:35:04 -0300

    Updates!

--- Nocturne/TableViewCell.swift
+++ Nocturne/TableViewCell.swift
@@ -7,18 +7,32 @@
 //
 
 import UIKit
+import QuartzCore
 
 class TableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    let gradientLayer = CAGradientLayer()
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("NSCoding not supported.")
     }
 
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
 
-        // Configure the view for the selected state
+        gradientLayer.frame = bounds
+        let color1 = UIColor(white: 1.0, alpha: 0.2).CGColor as CGColorRef
+        let color2 = UIColor(white: 1.0, alpha: 0.1).CGColor as CGColorRef
+        let color3 = UIColor.clearColor().CGColor as CGColorRef
+        let color4 = UIColor(white: 0.0, alpha: 0.1).CGColor as CGColorRef
+        gradientLayer.colors = [color1, color2, color3, color4]
+        gradientLayer.locations = [0.0, 0.01, 0.95, 1.0]
+        layer.insertSublayer(gradientLayer, atIndex: 0)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
     }
 
 }
