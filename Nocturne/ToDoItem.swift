5fb8b89017a52180b32cfc7ee6da9b2ccac004e6
Lucas <lucas@agtlucas.com>
2015-03-18 09:20:34 -0300

    Adding ToDoItem object

---
+++ Nocturne/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  Nocturne
+//
+//  Created by Lucas on 3/18/15.
+//  Copyright (c) 2015 AWLKA. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem: NSObject {
+   
+}
