2cd242edd7484b7ab86b7725fde8a7ea92125f69
Lucas <lucas@agtlucas.com>
2015-03-19 21:44:56 -0300

    Updates

--- Nocturne/TableViewCell.swift
+++ Nocturne/TableViewCell.swift
@@ -41,4 +41,25 @@ class TableViewCell: UITableViewCell {
         gradientLayer.frame = bounds
     }
 
+    func handlePan(recognizer: UIPanGestureRecognizer) {
+        if recognizer.state == .Began {
+            originalCenter = center
+        }
+
+        if recognizer.state == .Changed {
+            let translation = recognizer.translationInView(self)
+            center = CGPointMake(originalCenter.x + translation.x, originalCenter.y)
+
+            deleteOnDragRelease = frame.origin.x < -frame.size.width / 2.0
+        }
+
+        if recognizer.state == .Ended {
+            let originalFrame = CGRect(x: 0, y: frame.origin.y, width: bounds.size.width, height: bounds.size.height)
+
+            if !deleteOnDragRelease {
+                UIView.animateWithDuration(0.2, animations: {self.frame = originalFrame})
+            }
+        }
+    }
+
 }
